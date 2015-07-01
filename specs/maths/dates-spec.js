8d74ae9b507a60412771d8c42eda9448f7a96002
eloy_villasclaras <eloy.villasclaras@gmail.com>
2015-07-01 23:37:50 +0100

    d

--- specs/maths/dates-spec.js
+++ specs/maths/dates-spec.js
@@ -10,15 +10,43 @@ describe('dates', function () {
         b = {
             date: {year: 2004, month: 10, day: 10, hour: 12, minute: 0, second: 0},
             mjd: 53288.5
+        },
+        c = {
+            date: {year: 2015, month: 7, day: 1, hour: 23, minute: 10, second: 7},
+            mjd: 57204.96535880
+        },
+        d = {
+            date: {year: 1995, month: 4, day: 3, hour: 4, minute: 59, second: 55},
+            mjd: 49810.20827546
+        },
+
+        almostMjd = function (a, b) {
+            return Math.abs(a - b) < 1e-7;
+        },
+
+        almostDate = function (a, b) {
+            for (var key in b) {
+                if (b.hasOwnProperty(key)) {
+                    if (Math.abs(a[key] - b[key]) > Math.abs(b[key] * 1e-4)) {
+                        return key + ' ' + b[key] + ' | ' + a[key];
+                    }
+                }
+            }
+
+            return false;
         };
 
     it('should convert to mjd', function () {
         expect(dates.date2mjd(a.date)).to.equal(a.mjd);
         expect(dates.date2mjd(b.date)).to.equal(b.mjd);
+        expect(almostMjd(dates.date2mjd(c.date), c.mjd)).to.be.true;
+        expect(almostMjd(dates.date2mjd(d.date), d.mjd)).to.be.true;
     });
 
     it('should convert to date', function () {
-        expect(dates.date2mjd(a.mjd)).to.equal(a.date);
-        expect(dates.date2mjd(b.mjd)).to.equal(b.date);
+        expect(dates.mjd2date(a.mjd)).to.eql(a.date);
+        expect(dates.mjd2date(b.mjd)).to.eql(b.date);
+        expect(almostDate(dates.mjd2date(c.mjd), c.date)).to.be.false;
+        expect(almostDate(dates.mjd2date(d.mjd), d.date)).to.be.false;
     });
 });
--- src/js/maths/dates.js
+++ src/js/maths/dates.js
@@ -2,20 +2,44 @@ module.exports = function () {
     'use strict';
 
     var date2mjd = function (date) {
-        var a = date.month < 3 ? 1 : 0,
-            y = date.year + 4800 - a,
-            m = date.month + 12 * a - 3,
-            jdn = date.day + Math.floor((153 * m + 2) / 5)
-                + 365 * y + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(y / 400)
-                - 32045,
-            jd = jdn + (date.hour - 12) / 24 + date.minute / 1440 + date.second / 86400,
-            mjd = jd - 2400000.5;
+            var a = date.month < 3 ? 1 : 0,
+                y = date.year + 4800 - a,
+                m = date.month + 12 * a - 3,
+                jdn = date.day + Math.floor((153 * m + 2) / 5)
+                    + 365 * y + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(y / 400)
+                    - 32045,
+                jd = jdn + (date.hour - 12) / 24 + date.minute / 1440 + date.second / 86400;
 
-        return mjd;
-    };
+            return jd - 2400000.5;
+        },
+
+        mjd2date = function (mjd) {
+            var jd = mjd + 2400000.5,
+                j = Math.round(jd),
+                hms = (jd - j) * 86400,
+                ms = 43200 + hms,
+
+                f = j + 1401 + Math.floor(3 * Math.floor((4 * j + 274277) / 146097) / 4) - 38,
+                e = 4 * f + 3,
+                g = Math.floor((e % 1461) / 4),
+                h = 5 * g + 2,
+
+                month = (Math.floor(h / 153) + 2) % 12 + 1;
+
+            return {
+                year: Math.floor(e / 1461) - 4716 + Math.floor((14 - month) / 12),
+                month: month,
+                day: Math.floor((h % 153) / 5) + 1,
+                hour: 12 + Math.floor(hms / 3600),
+                minute: Math.floor(ms / 60) % 60,
+                second: ms % 60
+            };
+
+        };
 
     return {
-        date2mjd: date2mjd
-    }
+        date2mjd: date2mjd,
+        mjd2date: mjd2date
+    };
 
 }();
